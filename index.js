f9179f80081e1478be43de34428aec6e173aab16
thetamimahmed <wwwtamim000@gmail.com>
2023-06-09 03:20:15 +0600

    get all classes

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const classCollection = client.db("LLCDB").collection("classCollection");
+
+        app.get("/classes", async(req, res)=>{
+            const result = await classCollection.find().toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
