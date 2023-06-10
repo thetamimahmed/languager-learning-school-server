364bf460816f0c90b311937d77105039d8b2aa8b
thetamimahmed <wwwtamim000@gmail.com>
2023-06-10 13:08:35 +0600

    implement added class functionality

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
     if (!authorization) {
@@ -45,6 +46,7 @@ async function run() {
         const classCollection = client.db("LLCDB").collection("classCollection");
         const instructorCollection = client.db("LLCDB").collection("instructors");
         const bookingCollection = client.db("LLCDB").collection("bookingClasses");
+        const addedClassCollection = client.db("LLCDB").collection("addedClass");
 
         //jwt
         app.post("/jwt", (req, res) => {
@@ -62,6 +64,18 @@ async function run() {
             const result = await classCollection.find(query, options).toArray()
             res.send(result)
         })
+
+        //added pending class
+        app.post("/addedClass", async(req, res)=>{
+            const addedClass = req.body
+            if(addedClass.status === "pending"){
+                const result = await addedClassCollection.insertOne(addedClass)
+                return res.send(result)
+            }
+            const result = await classCollection.insertOne(addedClass)
+            res.send(result)   
+        })
+
         //get instructors
         app.get("/instructors", async (req, res) => {
             const query = {}
