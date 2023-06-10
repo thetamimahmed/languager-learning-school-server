869ffc9c0d961017eff6aaa1b7deb14fc86a54f2
thetamimahmed <wwwtamim000@gmail.com>
2023-06-10 11:30:20 +0600

    implement jwt

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId  } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if(err){
+            res.status(401).send({error:true, message:'Unauthorized Access'})
+        }
+        req.decoded = decoded
+        next()
+    });
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rgrretg.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,42 +46,56 @@ async function run() {
         const instructorCollection = client.db("LLCDB").collection("instructors");
         const bookingCollection = client.db("LLCDB").collection("bookingClasses");
 
+        //jwt
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send(token)
+        })
+
         //get classes
-        app.get("/classes", async(req, res)=>{
+        app.get("/classes", async (req, res) => {
             const query = {}
             const options = {
-                sort: {"total_enroll": -1}
+                sort: { "total_enroll": -1 }
             }
             const result = await classCollection.find(query, options).toArray()
             res.send(result)
         })
         //get instructors
-        app.get("/instructors", async(req, res)=>{
+        app.get("/instructors", async (req, res) => {
             const query = {}
             const options = {
-                sort: {"students_in_class": -1}
+                sort: { "students_in_class": -1 }
             }
             const result = await instructorCollection.find(query, options).toArray()
             res.send(result)
         })
 
         //booking class
-        app.post("/bookingclasses", async(req, res)=>{
+        app.post("/bookingclasses", async (req, res) => {
             const selectClass = req.body;
             const result = await bookingCollection.insertOne(selectClass)
             res.send(result)
         })
 
-        app.get("/bookingclasses", async(req, res)=>{
-            const email = req.query.email;
-            const query ={email: email}
+        app.get("/bookingclasses", verifyJWT, async (req, res) => {
+            const email = req.query?.email;
+            if(!email){
+                res.send([])
+            }
+            const decodedEmail = req.decoded?.email;
+            if(email !== decodedEmail){
+                return res.status(403).send({error: true, message: 'Forbidden Access'})
+            }
+            const query = { email: email }
             const result = await bookingCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.delete("/bookingclasses", async(req, res)=>{
+        app.delete("/bookingclasses", async (req, res) => {
             const id = req.query.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await bookingCollection.deleteOne(query)
             res.send(result)
         })
