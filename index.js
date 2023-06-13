44511e57e742c3b7efa62670957a8c10139835b6
thetamimahmed <wwwtamim000@gmail.com>
2023-06-13 13:34:56 +0600

    add payment history

--- index.js
+++ index.js
@@ -242,6 +242,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get("/payments", async(req, res)=>{
+            const result = await paymentCollection.find().toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
